39b73e01991ab2e032d62b5a48421e38cddc509a
Etienne Buschong <ebuschon@students.uni-mainz.de>
2020-03-30 19:25:13 +0200

    rework and finished content of education section

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,8 @@ window.onload = function () {
     const octagonHandler = new OctagonHandler();
     octagonHandler.register();
 
-    const cvHandler = new CVHandler();
-    cvHandler.register();
+   // const cvHandler = new CVHandler();
+    // cvHandler.register();
 
     const header = document.getElementsByClassName('header')[0];
     console.log("Header", window.getComputedStyle(header).clipPath);
